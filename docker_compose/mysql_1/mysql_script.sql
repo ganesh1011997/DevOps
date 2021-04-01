8aa2333f0570aeb3a7b785c492c103afaa4def72
ganesh1011997 <53637979+ganesh1011997@users.noreply.github.com>
2021-04-01 10:24:34 +0530

    Update mysql_script.sql

--- docker_compose/mysql_1/mysql_script.sql
+++ docker_compose/mysql_1/mysql_script.sql
@@ -1,1 +1,9 @@
+create user 'Ashwin'@'localhost' IDENTIFIED BY 'pucsd';
+CREATE DATABASE pucsdStudents;
+GRANT ALL PRIVILEGES ON pucsdStudents . * TO 'Ashwin'@'localhost';
+USE pucsdStudents;
 
+CREATE TABLE Students (name VARCHAR(20),Roll_no VARCHAR(20),address text,mobile_number BIGINT,pan_number varchar(20));
+
+INSERT INTO Students VALUES ('Ashwin Shelke','18146','Laxmi road','8605354555','rre222889');
+1
