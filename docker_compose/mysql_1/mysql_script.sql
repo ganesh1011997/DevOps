36a22a447cd64cc367fb0ea63f154e7bbaf494e1
ganesh1011997 <53637979+ganesh1011997@users.noreply.github.com>
2021-04-01 10:37:49 +0530

    Update mysql_script.sql

--- docker_compose/mysql_1/mysql_script.sql
+++ docker_compose/mysql_1/mysql_script.sql
@@ -6,4 +6,4 @@ USE pucsdStudents;
 CREATE TABLE Students (name VARCHAR(20),Roll_no VARCHAR(20),address text,mobile_number BIGINT,pan_number varchar(20));
 
 INSERT INTO Students VALUES ('Ashwin Shelke','18146','Laxmi road','8605354555','rre222889');
-1
+
